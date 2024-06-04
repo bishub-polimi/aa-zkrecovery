28154d77ce60bc086eb633d8e269860415923256
Marco Esposito <gufett0@tutanota.com>
2024-06-04 11:15:14 +0200

    setting up

---
+++ packages/circuits/helpers/generate-inputs.ts
@@ -0,0 +1,5 @@
+import { bytesToBigInt, fromHex } from "@zk-email/helpers/dist/binaryFormat";
+import { generateEmailVerifierInputs } from "@zk-email/helpers/dist/input-generators";
+
+export const STRING_PRESELECTOR = "";
+
